b3f2f873164fa1277f882b8b4c6c07b5d715a91f
NoRiceOnigiri <nomorethensand@gmail.com>
2024-12-15 18:59:46 +0300

    Simple menu without ref.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 
 int main()
 {
+    do {
+        std::cout << "--- Обучайка ---" << std::endl;
+        
+        std::cout << "1. Изучать программирование." << std::endl;
+        std::cout << "0. Пойти играть в футбол." << std::endl;
+
+        std::cout << "Введите ответ: ";
+        int answer;
+        std::cin >> answer;
+
+        if (answer == 0){
+            exit(0);
+        }
+
+        std::cout << std::endl;
+    } while (true);
+
     return 0;
 }
