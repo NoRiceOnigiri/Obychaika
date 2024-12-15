502f34895be6ba0d3dd581936ce69ba6097b8df9
NoRiceOnigiri <nomorethensand@gmail.com>
2024-12-15 20:21:33 +0300

    few changes

--- main.cpp
+++ main.cpp
@@ -5,21 +5,25 @@
 
 int main()
 {
-    Menu::MenuItem main_study {"1. Изучать программирование.", Functions::main_play_football};
+    Menu::MenuItem main_study {"1. Изучать программирование.", Functions::main_study};
     Menu::MenuItem main_play_football {"0. Пойти играть в футбол.", Functions::main_play_football};
 
+    Menu::MenuItem* options [] = {&main_play_football, &main_study};
+    const int options_count = sizeof(options)/sizeof(options[0]);
     do {
         std::cout << "--- Обучайка ---" << std::endl;
         
-        std::cout << (&main_study)->title << std::endl;
-        std::cout << (&main_play_football)->title << std::endl;
-
-        std::cout << "Введите ответ: ";
+        for (int i = 1; i < options_count; i++)
+            std::cout << options[i]->title << std::endl;
+        std::cout << options[0]->title << std::endl;
+        
         int answer;
-        std::cin >> answer;
-        if (answer == 0){
-           (&main_play_football)->func();
-        }
+        do
+        {
+            std::cout << "Введите ответ: ";
+            std::cin >> answer;
+        } while (answer < 0 || answer >= options_count);
+        options[answer]->func();
 
         std::cout << std::endl;
     } while (true);
